bd287b0778706d063d16d6498ec3be45578bcf5c
dc <yomaybedc@gmail.com>
2019-08-22 00:02:17 +0800

    添加直播播放器的播放代码

--- safe-school-frontend-v2/js/live.modal.js
+++ safe-school-frontend-v2/js/live.modal.js
@@ -1,31 +1,40 @@
 function show_live(did) {
-    console.log('show_live',did)
-    // var did = $(this).attr('id')
-    // 创建一个弹出框.
+    console.log('show_live', did)
+        // var did = $(this).attr('id')
+        // 创建一个弹出框.
     $('#exampleModa2').modal('show')
-    $('#exampleModa2').find('img').attr('id', 'modal-img-live-' + did)
+        // $('#exampleModa2').find('img').attr('id', 'modal-img-live-' + did)
     socket.emit('dolive', { did: did, opt: 1 });
     $('#qidong').show();
-    socket.on(did, function (msg) {
-        $('#qidong').hide();
-        var value = msg.did;
-        $('#modal-img-live-' + value).attr('src', 'data:image/jpg;base64,' + msg.data);
-    })
-    let response_res=sessionStorage.getItem('response_res');
-    let d=JSON.parse(response_res)
+    // socket.on(did, function(msg) {
+    //     $('#qidong').hide();
+    //     var value = msg.did;
+    //     $('#modal-img-live-' + value).attr('src', 'data:image/jpg;base64,' + msg.data);
+    // })
+    var player = document.getElementById('videoElement');
+    var flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        isLive: true,
+        hasAudio: false,
+        url: 'http://192.168.1.249:8080/live/livestream.flv' //srs播放地址
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    let response_res = sessionStorage.getItem('response_res');
+    let d = JSON.parse(response_res)
     console.log(d)
     console.log(did)
-    d.forEach(e=>{
+    d.forEach(e => {
 
-        if(did==e.did){
-            console.log(e)
-             $('.video_title').html(e.location);
-        }
-    })
-       //
+            if (did == e.did) {
+                console.log(e)
+                $('.video_title').html(e.location);
+            }
+        })
+        //
 }
- // 直播弹窗关闭
- $('#exampleModa2').on('hide.bs.modal', function(e) {
+// 直播弹窗关闭
+$('#exampleModa2').on('hide.bs.modal', function(e) {
     var img = $('#exampleModa2').find('img')
     img.attr('src', '')
     var s_id = img.attr('id')
@@ -38,7 +47,7 @@ function show_live(did) {
 //     var imgs = $('#exampleModal1').find('img');
 //     // console.log(imgs)
 //     imgs.forEach(e => {
-        
+
 //     });
 //     // imgs.attr('src', '')
 //     // var s_id = img.attr('class')
